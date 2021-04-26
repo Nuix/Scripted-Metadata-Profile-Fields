7eab323ee04c9db4076252d75e8c5266317d3f73
Michael Kamida <michael.kamida@nuix.com>
2021-04-26 10:52:59 -1000

    Create ScriptedField_TermCount.rb
    Yields the number of occurrences of a term in the item's text

---
+++ Ruby/QC/ScriptedField_TermCount.rb
@@ -0,0 +1,10 @@
+# Yields the number of occurrences  of a term in the item's text
+term = 'deadbeef'
+# Get count from term statistics for this item
+case_stats = $current_case.get_statistics
+term_stats = case_stats.get_term_statistics("guid:#{$current_item.get_guid}", 'field' => 'content')
+count = term_stats[term]
+# Return 0 if term is not included
+return 0 if count.nil?
+
+count
