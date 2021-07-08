9bf952157daa7467df33add04130b1550f7deab5
Jason Wells <jason.wells@nuix.com>
2021-07-08 13:28:48 -0700

    added fields for resolving ancestor container names

---
+++ Ruby/Pathing/ScriptedField_ImmediateContainerAncestorName.rb
@@ -0,0 +1,19 @@
+# Finds the most immediate ancestor container and yields its name, excluding
+# Nuix evidence container items.  Yields a blank value if none is found.
+
+path_items = $current_item.getPath
+path_items.reverse_each do |item|
+	# Don't report this item if it is a container
+	next if item == $current_item
+
+	kind = item.getKind.getName
+	if kind.downcase.strip == "container"
+		# We don't want to report evidence container name
+		next if item.getType.getName == "application/vnd.nuix-evidence"
+		
+		return item.getLocalisedName
+	end
+end
+
+# If we reached here we didn't find one above
+return ""
