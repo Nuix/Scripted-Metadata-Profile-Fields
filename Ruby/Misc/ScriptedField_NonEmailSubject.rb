c8bf8c010f7a5355dfe46b0435dae6b1f259e8c5
Jason Wells <jason.wells@nuix.com>
2021-07-08 10:30:41 -0700

    subject field that only yields value for non emails that have the property

---
+++ Ruby/Misc/ScriptedField_NonEmailSubject.rb
@@ -0,0 +1,16 @@
+# Reports subject only items which are not an email
+
+kind = $current_item.getKind.getName
+if kind.downcase.strip == "email"
+	return ""
+else
+	# Used to find subject key in case it has odd capitalization
+	subject_pattern = /^subject$/i
+	properties = $current_item.getProperties
+	
+	# Find key which is "subject" (case insensitive)
+	subject_key = properties.keys.find{|k|k =~ subject_pattern}
+	
+	# Return the subject property or blank if nil somehow
+	return properties[subject_key] || ""
+end
