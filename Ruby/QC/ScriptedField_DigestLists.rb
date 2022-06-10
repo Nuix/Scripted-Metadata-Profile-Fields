c297303756ea99d419ca5bba8cc47e256ee1f449
Michael Kamida <22751246+nuix-mrk@users.noreply.github.com>
2022-06-09 15:34:01 -1000

    note on matches_search() in compound cases

--- Ruby/QC/ScriptedField_DigestLists.rb
+++ Ruby/QC/ScriptedField_DigestLists.rb
@@ -1,5 +1,6 @@
 DELIMITER = '; '
 lists = $utilities.get_digest_list_store.get_digest_list_names.select do |name|
   $current_item.matches_search("digest-list:\"#{name}\"")
+  # this may error in a compound case, if the digest list exists only in the compound case (and not in the user or simple case), as matches_search() runs in the simple case.
 end
 lists.sort.join(DELIMITER)
